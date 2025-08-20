0042eaeb7ccc06d8add14c4a314d39eed0199e9b
MariPinas <marianapls2003@gmail.com>
2025-08-20 04:09:20 -0300

    :sparkles: crud administrador e veiculo

---
+++ Dominio/ModelViews/Home.cs
@@ -0,0 +1,6 @@
+namespace MinimalApi.Dominio.ModelViews;
+
+public struct Home{
+    public string Mensagem{get=> "Bem Vindo a API de veiculos - Minimal API";}
+    public string Doc {get => "/swagger";}
+}
